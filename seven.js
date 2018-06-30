f0e11ff9e0a72b0661fc4727124b8096304366f5
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-30 13:26:31 -0700

    seven done

--- seven.js
+++ seven.js
@@ -1,6 +1,21 @@
 //Write a function max that accepts an array of numbers and returns the largest number in the array. 
 
 var max = function (nums) {
-    //find length of nums
+    //declare var largestNum
+    var largestNum = nums[0];
+    //iterate through nums with a for-loop
+    for (var i = 0; i < nums.length; i++) {    //i can be started wherever yuo want  
+        if (nums[i] > largetsNum) {
+            largestNum = nums[i];
+        }
+    }
+    //find length of nums; this will tell us when to stop loop 
     //look at val in array 
+    //compare the vals; which val is greater
+    //if num greatet than other, it becomes largestNum
+    return largestNum
 }
+
+max([1, 6, 8, 9, 100])
+
+//this works!! 
