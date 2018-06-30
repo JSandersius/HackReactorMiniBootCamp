db777c7066871a75db2c0399f7fd0ac2c5c18a1e
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-30 15:16:43 -0700

    added sixteen

---
+++ NotesFromHackReactor/sixteen.js
@@ -0,0 +1,21 @@
+//rewrite of fifteen using each 
+
+var numbers = [1, 9, 27, 5, 10, 3];
+var sum = 0;
+
+
+
+for (var i = 0; i < numbers.length; i++) {
+    product = product * numbers[i];
+    //seperate function 
+}
+
+var nums = [7, 8, 9, 10]
+
+var each = function (array, func) {
+    for (var i = 0; i < array.length; i++) {
+        func(array[i]);
+    }
+}
+each(nums)
+
