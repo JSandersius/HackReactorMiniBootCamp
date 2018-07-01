9776575c2a0c6ba0e0f93f814e814c5ed261f372
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-01 13:18:14 -0700

    last notes in this repo for now

---
+++ NotesFromHackReactor/seventeen.js
@@ -0,0 +1,14 @@
+// Hack reactor BootCamp? 
+//hackreactor/events --> > > > > > > > > < < < < < < < < < > > > > > > > > > > < < < < < < > >  >> > > > 
+// prepare for technical interview to get it, the pre-course work 
+// then entrance into 13 week program --> < < > >  the three months to land a job >>>><<<<<  
+
+//Hackreactor/prepare  
+//very javascript based 
+//after bootcamp, job searching and further knowledge and networking and bombing inteviews
+// cracking code interview --> 
+//whiteboarding --> all in-house 
+// is the  job market saturated? there are more bootcamp grads then two years ago. 
+// previous work experience? elavator pitch for that 
+// data structure/ algorithms --> --> --> 
+//domain knowledge in Javascript --> 
